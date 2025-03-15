76ad870e3491d575427dff03bf4c129d5a17ddd8
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2025-03-15 11:55:24 -0300

    Update index.ts

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -9,3 +9,24 @@ export function removeQueryUrl() {
     const url = window.location.href
     window.history.pushState({}, document.title, url.split('?')[0])
 }
+
+export function $<T extends HTMLElement>(
+    q: string,
+    c: Element = document as any,
+) {
+    return c.querySelector(q) as T
+}
+
+export function $$<T extends HTMLElement>(
+    q: string,
+    c: Element = document as any,
+) {
+    return Array.from(c.querySelectorAll(q)) as T[]
+}
+
+export function once(el: Element, evt: keyof ElementEventMap): Promise<any>
+export function once(el: Element, evt: string) {
+    return new Promise((done) =>
+        el.addEventListener(evt, done, { once: true }),
+    )
+}
